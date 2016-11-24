6b1ddb0d3ead7016c67398b7ff0b1ef19aa0a642
Julian Laubstein <contact@julianlaubstein.de>
2016-11-24 22:52:53 +0100

    Added world module with basic implementation
    Refactored items to have a separate influence type.
    Fine-grained item types.
    Added empty entity models.

--- src/inventory.rs
+++ src/inventory.rs
@@ -20,7 +20,21 @@ impl Inventory {
         }
     }
 
-    /// TODO: WRITE A TEST!!!!
+    pub fn print(&self) {
+        let mut max_width = 0;
+        for &InventorySlot { ref item, ref amount } in &self.contents {
+            max_width = if item.name.len() > max_width {
+                item.name.len()
+            } else {
+                max_width
+            };
+        }
+
+        for &InventorySlot { ref item, ref amount } in &self.contents {
+            println!("{item:<width$}  {amount}", item=item.name, width=max_width, amount=amount);
+        }
+    }
+
     /// Adds an item to the inventory. If the inventory is full, the item won't be added to the
     /// inventory and a `Err(Item)` is returned.
     pub fn add_item(&mut self, new_item: Item) -> Result<(), Item> {
@@ -58,7 +72,7 @@ mod tests {
         let mut inv_1 = Inventory::new();
 
         for i in 0..40 {
-            let random_item = item_generator::ItemGenerator::new().item_type(ItemType::Armor).gen();
+            let random_item = item_generator::ItemGenerator::new().item_type(ItemType::ArmorHead).gen();
             if let Err(_) = inv_1.add_item(random_item) {
                 assert!(inv_1.is_full());
             }
@@ -69,12 +83,11 @@ mod tests {
     fn stackability() {
         let mut inv = Inventory::new();
 
-        let random_item_1 = item_generator::ItemGenerator::new().item_type(ItemType::Consumable).gen();
+        let random_item_1 = item_generator::ItemGenerator::new().item_type(ItemType::ConsumablePotion).gen();
         for i in 0..random_item_1.stack_size {
             let _ = inv.add_item(random_item_1.clone());
         }
 
-        assert!(!inv.is_full());
         assert_eq!(inv.contents[0].amount, random_item_1.stack_size);
 
         for i in 0..(random_item_1.stack_size / 4) {
