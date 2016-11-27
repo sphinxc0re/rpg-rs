524e2b55520c76ff626505af170a3f10482e047d
Julian Laubstein <contact@julianlaubstein.de>
2016-11-27 23:45:17 +0100

    Added documentation to event

--- src/event.rs
+++ src/event.rs
@@ -1,13 +1,22 @@
 use item::Item;
 
+/// A primitive event type
 #[derive(Clone)]
 pub enum Event {
+    /// Talk to an entity
     Tell(String),
+    /// Give an item to an entity
     Give(Item),
+    /// Show a list events an entity can handle
     Options(Vec<Event>),
+    /// Push an entity
     Push,
+    /// Pull an entity
     Pull,
+    /// Open an entity
     Open,
+    /// Close an entity
     Close,
+    /// Do nothing
     Nothing,
 }
