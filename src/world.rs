4ab2e8a2040b25604ae5c6586ba291fd719ad6ce
Julian Laubstein <julianlaubstein@yahoo.de>
2016-11-29 08:36:56 +0100

    Added save methods to the world mod (#11)
    * Added save methods to the world mod
    
    * Refactored code

--- src/world.rs
+++ src/world.rs
@@ -1,16 +1,20 @@
-use entity::Entity;
+use rustc_serialize::json;
+use std::io::prelude::*;
+use std::fs::File;
 
 /// A single field of the world
+#[derive(RustcEncodable, RustcDecodable)]
 pub struct Field {
     /// The type of the field
-    field_type: FieldType,
+    pub field_type: FieldType,
     /// The height of the field. Used for collision detection
-    height: u8,
-    /// The contained entity (optional)
-    contained_entity: Option<Entity>,
+    pub height: u8,
+    /// The id if the contained entity (optional)
+    pub contained_entity: Option<usize>,
 }
 
 /// The field type. Used to determine the optical properties of the ground
+#[derive(RustcEncodable, RustcDecodable)]
 pub enum FieldType {
     /// A field consists of dirt
     Dirt,
@@ -41,19 +45,61 @@ pub enum FieldType {
 /// A larger section of a campagne containing a starting point and end point. The starting point
 /// is where the character *spawns* and the end point is the point he has to reach for the next
 /// level to begin.
+#[derive(RustcEncodable, RustcDecodable)]
 pub struct Level {
     /// The name or title of the level
-    name: String,
+    pub name: String,
     /// The entry point of the character
-    starting_point: (usize, usize),
+    pub starting_point: (usize, usize),
     /// The point where the level is finished
-    end_point: (usize, usize),
+    pub end_point: (usize, usize),
     /// The actual fields, the level consists of
     data: Vec<Vec<Field>>,
 }
 
 /// A collection of levels. Usually used to create larger adventures
+#[derive(RustcEncodable, RustcDecodable)]
 pub struct Campagne {
-    name: String,
+    /// The title of the campagne
+    pub title: String,
     levels: Vec<Level>,
+}
+
+impl Campagne {
+    /// Saves the campagne to the specified file
+    pub fn save_to_file(&self, file_name: &str) {
+        let mut f = match File::create(file_name) {
+            Err(_) => return,
+            Ok(file) => file,
+        };
+
+        let campagne = match json::encode(self) {
+            Err(_) => return,
+            Ok(campagne) => campagne,
+        };
+
+        match f.write_all(campagne.as_bytes()) {
+            Err(_) => {},
+            Ok(_) => {},
+        };
+    }
+
+    /// Loads the campagen from the specified file
+    pub fn load_from_file(file_name: &str) -> Result<Campagne, &str> {
+        let mut f = match File::open(file_name) {
+            Err(_) => return Err(file_name),
+            Ok(file) => file,
+        };
+
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Err(_) => return Err(file_name),
+            Ok(_) => {},
+        };
+
+        match json::decode(s.as_str()) {
+            Err(_) => return Err(file_name),
+            Ok(campagne) => Ok(campagne),
+        }
+    }
 }
