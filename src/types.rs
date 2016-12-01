0a08a60700cf9b75b77c3b6b10d279ca540b3e15
Julian Laubstein <contact@julianlaubstein.de>
2016-12-01 17:52:14 +0100

    Added a new type EntityMap

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use entity::Entity;
+
 /// Everything that can *interact with* or *be* the value of an attribute.
 pub type AttributeValue = i64;
 
@@ -6,3 +9,6 @@ pub type Health = usize;
 
 /// The type for gold. Used as a currency.
 pub type Gold = usize;
+
+/// A map mapping entity_ids to actual entities
+pub type EntityMap = HashMap<usize, Entity>;
