4b80c20ee52ddbd95609aa7bf55aabaa1ba90a22
Julian Laubstein <contact@julianlaubstein.de>
2017-01-23 23:01:43 +0100

    Added tests

--- src/engine.rs
+++ src/engine.rs
@@ -19,38 +19,47 @@ impl EngineContext {
 
 /// The engine to run the game
 pub struct Engine {
-    setup: Box<Fn(EngineContext) -> EngineContext>,
-    update: Box<Fn(EngineContext) -> EngineContext>,
-    draw: Box<Fn(EngineContext) -> EngineContext>,
+    setup: Option<Box<Fn(EngineContext) -> EngineContext>>,
+    update: Option<Box<Fn(EngineContext) -> EngineContext>>,
+    draw: Option<Box<Fn(EngineContext) -> EngineContext>>,
 }
 
 impl Engine {
+    /// Creates a new engine
+    pub fn new() -> Engine {
+        Engine {
+            setup: None,
+            update: None,
+            draw: None,
+        }
+    }
+
     /// A method to define the setup behavior
     pub fn setup<T: 'static>(&mut self, setup: T)
         where T: Fn(EngineContext) -> EngineContext
     {
-        self.setup = Box::new(setup);
+        self.setup = Some(Box::new(setup));
     }
 
     /// A method to define the update behavior
     pub fn update<T: 'static>(&mut self, update: T)
         where T: Fn(EngineContext) -> EngineContext
     {
-        self.update = Box::new(update);
+        self.update = Some(Box::new(update));
     }
 
     /// A method to define the draw behavior
     pub fn draw<T: 'static>(&mut self, draw: T)
         where T: Fn(EngineContext) -> EngineContext
     {
-        self.draw = Box::new(draw);
+        self.draw = Some(Box::new(draw));
     }
 
     /// Start the engine
     pub fn start(&self) {
-        let setup = &self.setup;
-        let update = &self.update;
-        let draw = &self.draw;
+        let setup = self.setup.as_ref().unwrap();
+        let update = self.update.as_ref().unwrap();
+        let draw = self.draw.as_ref().unwrap();
 
         let mut context = EngineContext::new();
         context = setup(context);
@@ -66,5 +75,42 @@ impl Engine {
                 context = draw(context);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn workflow() {
+        let mut engine = Engine::new();
+
+        engine.setup(|context| {
+            // Setup your game
+            // ...
+            // ..
+            // .
+
+            // Return the altered/non-altered context
+            context
+        });
+
+        engine.update(|context| {
+            // Implement your update mechanics
+
+            // Return the altered/non-altered context
+            context
+        });
+
+        engine.draw(|context| {
+            // Implement your output
+
+            // Return the altered/non-altered context
+            context
+        });
+
+        // Start the engine => run the game
+        engine.start();
     }
 }
