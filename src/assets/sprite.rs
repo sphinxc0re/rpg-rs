40e26ba4ec63d52a3b4c07e60f742404b67f0a7f
Julian Laubstein <contact@julianlaubstein.de>
2017-03-16 20:28:20 +0100

    Added Sprite Asset

--- src/assets/sprite.rs
+++ src/assets/sprite.rs
@@ -1,7 +1,24 @@
+use errors::*;
+use super::Asset;
+use std::path::Path;
+
 /// A sprite that can be drawn onto a graphic context
 pub struct Sprite {
     filename: String,
     width: u32,
     height: u32,
     content: Vec<u8>,
+}
+
+impl Asset for Sprite {
+    fn load<P: AsRef<Path>>(path: P) -> Result<Self>
+        where Self: ::std::marker::Sized
+    {
+        Ok(Sprite {
+            filename: stringify!(path).to_string(),
+            width: 50,
+            height: 50,
+            content: Vec::new(),
+        })
+    }
 }
